b98698ee6bd6b71fac1cb8f23aaf692920b29014
YangVincent <VincentYang2014@gmail.com>
2014-12-28 20:20:19 -0800

    main setup for jframe

--- Chess/src/ChessFrame.java
+++ Chess/src/ChessFrame.java
@@ -2,6 +2,25 @@ import javax.swing.JFrame;
 
 
 public class ChessFrame extends JFrame {
+	//fields
+	
+	
+	//constructors
+	public ChessFrame(String name)
+	{
+		super(name);
+	}
+	
+	//methods
+	public static void main(String[] args)
+	{
+		System.out.println("starting the main");
+		
+		ChessFrame game = new ChessFrame("Chess");//pass in players or ai
+		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.setVisible(true);
+		System.out.println("created");
+	}
 	boolean canMove()
 	{
 		return true;
@@ -14,4 +33,5 @@ public class ChessFrame extends JFrame {
 	{
 		return 1;
 	}
+	
 }
